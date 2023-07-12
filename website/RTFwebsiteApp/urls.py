ff9344ead72b7dd0fc95337d752116147f7849e4
Estoneccc <kutyavin.danil@mail.ru>
2023-07-12 23:18:09 +0500

    исправил ошибки, добавил фильтрацию по специальностям

--- website/RTFwebsiteApp/urls.py
+++ website/RTFwebsiteApp/urls.py
@@ -1,16 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index.html', views.main_page, name='main_page'),
+    path('', views.main_page, name='main_page'),
     path('reference-info.html', views.information, name='information'),
-    path('acticles.html', views.article_list, name='article_list'),
+    path('articles.html', views.article_list, name='article_list'),
     path('article/<int:pk>/', views.article_detail, name='article_detail'),
     path('employment.html/', views.employment, name='employment'),
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
